30ed239eebf8249c7ca2b71cb72ccb7717eb17af
qwe1111-sketch <3428001411@qq.com>
2026-02-23 14:03:13 +0800

    Update app_localizations_en.dart

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,7 +9,7 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get appTitle => 'Sport Flutter';
+  String get appTitle => 'HYKOIUU';
 
   @override
   String get upNext => 'Up Next';
